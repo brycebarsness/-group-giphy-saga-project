f18edf7e091b4ddeb1c0d22452ae0a80c29e8dd1
Quinn Johnson <johnson.fargo87@gmail.com>
2021-02-12 08:50:57 -0600

    styling changes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,25 +2,27 @@ import SearchGifs from '../SearchGifs/SearchGifs'
 import GifList from '../GifList/GifList'
 import React from "react";
 import FavoriteView from "../FavoriteView/FavoriteView";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
+import './App.css'
 
 // BrowserRouter as
 
 function App(props) {
   return (
-    <div>
-      <h1>Giphy Search!</h1>
-      <SearchGifs />
-      <GifList />
       <Router>
-        <h1>Giphy Search!</h1>
-
-        <div className="content-container">
-          <Route path="/favorites" component={FavoriteView} />
-          <FavoriteView />
-        </div>
+        <header>
+          <h1>Giphy Search!</h1>
+          <ul>
+            <li><Link to='/search'>Search</Link></li>
+            <li><Link to='/favorites'>Favorites</Link></li>
+          </ul>
+        </header>
+          <Switch>
+            <Route path='/search' component={SearchGifs}></Route>
+            <Route path='/searchlist' component={GifList}></Route>
+            <Route path="/favorites" component={FavoriteView}></Route>
+          </Switch>
       </Router>
-    </div>
   );
 }
 
